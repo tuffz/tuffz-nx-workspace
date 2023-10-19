78b47ce19156b5d4d1da90f12201f65f1e28da6b
Eric Büttner <eric.buettner@tuffz.com>
2023-10-19 17:42:15 +0200

    chore(footer): add placeholder content for tuffz.com

--- libs/shared/ui/footer/src/lib/shared-ui-footer.tsx
+++ libs/shared/ui/footer/src/lib/shared-ui-footer.tsx
@@ -4,7 +4,24 @@ import styles from './shared-ui-footer.module.scss';
 export interface SharedUiFooterProps {}
 
 export function SharedUiFooter(props: SharedUiFooterProps) {
-  return <div className="sticky top-[100vh]">SharedUiFooter</div>;
+  return (
+    <div className="sticky top-[100vh] py-10">
+      <p className="text-center">
+        Carefully made with{' '}
+        <span role="img" aria-label="Heart">
+          ❤️
+        </span>{' '}
+        in{' '}
+        <a
+          href="https://www.tuffz.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          tuffz.com
+        </a>
+      </p>
+    </div>
+  );
 }
 
 export default SharedUiFooter;
