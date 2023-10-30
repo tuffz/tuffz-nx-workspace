651196e713bf1561619a3f4b4d49850b4777cc47
Eric Büttner <eric.buettner@tuffz.com>
2023-10-30 10:47:38 +0100

    chore(tuffz-info): remove nx-welcome component

--- apps/tuffz-info/src/app/app.tsx
+++ apps/tuffz-info/src/app/app.tsx
@@ -1,14 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
 import { SharedUiFooter } from 'shared-ui-footer';
 
 export function App() {
   return (
     <div id="app" className="min-h-screen">
-      <NxWelcome title="tuffz-info" />
-
       <SharedUiFooter />
     </div>
   );
