3737a2a8fb71a05c23016a9b441b9b9b837c79e3
Eric Büttner <eric.buettner@tuffz.com>
2023-11-05 19:45:11 +0100

    feat(shared-ui-anchor): add `rel` attribute logic for use case target="_blank"

--- libs/shared/ui/anchor/src/lib/shared-ui-anchor.spec.tsx
+++ libs/shared/ui/anchor/src/lib/shared-ui-anchor.spec.tsx
@@ -1,6 +1,8 @@
 import { render } from '@testing-library/react';
 
-import SharedUiAnchor from './shared-ui-anchor';
+import { SharedUiAnchor } from './shared-ui-anchor';
+import { SharedUiAnchorProps } from './shared-ui-anchor';
+import { setRelAttribute } from './shared-ui-anchor';
 
 describe('SharedUiAnchor', () => {
   it('renders a link with the provided href', () => {
@@ -42,5 +44,84 @@ describe('SharedUiAnchor', () => {
     );
     const link = getByRole('link');
     expect(link).toHaveAttribute('rel', 'nofollow');
+  });
+
+  it('renders the rel attribute for _blank target', () => {
+    const { getByRole } = render(
+      <SharedUiAnchor href="https://www.tuffz.com" target="_blank" />
+    );
+    const link = getByRole('link');
+    expect(link).toHaveAttribute('rel', 'noopener noreferrer');
+  });
+
+  it('renders the provided rel and the rel attribute for _blank target', () => {
+    const { getByRole } = render(
+      <SharedUiAnchor
+        href="https://www.tuffz.com"
+        target="_blank"
+        rel="nofollow"
+      />
+    );
+    const link = getByRole('link');
+    expect(link).toHaveAttribute('rel', 'nofollow noopener noreferrer');
+  });
+
+  it('renders the provided rel and the rel attribute for _blank target and makes sure that the rel attributes for _blank are not doubled in rel', () => {
+    const { getByRole } = render(
+      <SharedUiAnchor
+        href="https://www.tuffz.com"
+        target="_blank"
+        rel="noopener"
+      />
+    );
+    const link = getByRole('link');
+    expect(link).toHaveAttribute('rel', 'noopener noreferrer');
+  });
+
+  it('does not render the rel attribute for non-_blank target', () => {
+    const { getByRole } = render(
+      <SharedUiAnchor href="https://www.tuffz.com" />
+    );
+    const link = getByRole('link');
+    expect(link).not.toHaveAttribute('rel');
+  });
+});
+
+describe('setRelAttribute', () => {
+  it('sets rel attribute to "noopener noreferrer" when target is "_blank"', () => {
+    const props: SharedUiAnchorProps = {
+      href: 'https://www.tuffz.info',
+      target: '_blank',
+    };
+    let rel: string | null = null;
+
+    rel = setRelAttribute(props);
+
+    expect(rel).toBe('noopener noreferrer');
+  });
+
+  it('merges props.rel with rel when target is "_blank" and props.rel is defined', () => {
+    const props: SharedUiAnchorProps = {
+      href: 'https://www.tuffz.info',
+      target: '_blank',
+      rel: 'nofollow',
+    };
+    let rel: string | null = null;
+
+    rel = setRelAttribute(props);
+
+    expect(rel).toBe('nofollow noopener noreferrer');
+  });
+
+  it('sets rel attribute to props.rel when target is not "_blank"', () => {
+    const props: SharedUiAnchorProps = {
+      href: 'https://www.tuffz.info',
+      rel: 'noopener noreferrer',
+    };
+    let rel: string | null = null;
+
+    rel = setRelAttribute(props);
+
+    expect(rel).toBe('noopener noreferrer');
   });
 });
