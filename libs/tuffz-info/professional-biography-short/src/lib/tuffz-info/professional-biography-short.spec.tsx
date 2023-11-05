43c17136771f661d7542f69f75e4d6b9253471a8
Eric Büttner <eric.buettner@tuffz.com>
2023-11-05 22:26:34 +0100

    test(tuffz-info-professional-biography-short): add a test that guarantees that the image is rendered

--- libs/tuffz-info/professional-biography-short/src/lib/tuffz-info/professional-biography-short.spec.tsx
+++ libs/tuffz-info/professional-biography-short/src/lib/tuffz-info/professional-biography-short.spec.tsx
@@ -1,10 +1,18 @@
 import { render } from '@testing-library/react';
-
 import ProfessionalBiographyShort from './professional-biography-short';
 
 describe('ProfessionalBiographyShort', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<ProfessionalBiographyShort />);
-    expect(baseElement).toBeTruthy();
+  it('renders the component with the expected structure and content', () => {
+    const { getByAltText } = render(<ProfessionalBiographyShort />);
+
+    // Check that the image is rendered with the correct attributes
+    const image = getByAltText('Portrait of Eric Büttner');
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute(
+      'src',
+      '/assets/personal-information/eric-buettner.jpeg'
+    );
+    expect(image).toHaveAttribute('alt', 'Portrait of Eric Büttner');
+    expect(image).toHaveAttribute('title', 'Portrait of Eric Büttner');
   });
 });
