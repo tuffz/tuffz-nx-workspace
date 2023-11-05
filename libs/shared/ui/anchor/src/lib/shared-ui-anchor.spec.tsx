35e2790dffded42417a092c3d7b115ece70c1215
Eric Büttner <eric.buettner@tuffz.com>
2023-11-05 18:03:55 +0100

    refactor(shared-ui-anchor)!: rename property `body` to `content`
    BREAKING CHANGE: rename property of the link content/text from `body` to `content`

--- libs/shared/ui/anchor/src/lib/shared-ui-anchor.spec.tsx
+++ libs/shared/ui/anchor/src/lib/shared-ui-anchor.spec.tsx
@@ -3,8 +3,43 @@ import { render } from '@testing-library/react';
 import SharedUiAnchor from './shared-ui-anchor';
 
 describe('SharedUiAnchor', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<SharedUiAnchor />);
-    expect(baseElement).toBeTruthy();
+  it('renders a link with the provided href', () => {
+    const { getByRole } = render(
+      <SharedUiAnchor href="https://www.tuffz.com" />
+    );
+    const link = getByRole('link');
+    expect(link).toHaveAttribute('href', 'https://www.tuffz.com');
+  });
+
+  it('renders the provided content text', () => {
+    const { getByText } = render(
+      <SharedUiAnchor href="https://www.tuffz.com" content="Link Text" />
+    );
+    const link = getByText('Link Text');
+    expect(link).toBeInTheDocument();
+  });
+
+  it('renders the provided title attribute', () => {
+    const { getByRole } = render(
+      <SharedUiAnchor href="https://www.tuffz.com" title="Link Title" />
+    );
+    const link = getByRole('link');
+    expect(link).toHaveAttribute('title', 'Link Title');
+  });
+
+  it('renders the target attribute', () => {
+    const { getByRole } = render(
+      <SharedUiAnchor href="https://www.tuffz.com" target="_blank" />
+    );
+    const link = getByRole('link');
+    expect(link).toHaveAttribute('target', '_blank');
+  });
+
+  it('renders the provided rel attribute', () => {
+    const { getByRole } = render(
+      <SharedUiAnchor href="https://www.tuffz.com" rel="nofollow" />
+    );
+    const link = getByRole('link');
+    expect(link).toHaveAttribute('rel', 'nofollow');
   });
 });
