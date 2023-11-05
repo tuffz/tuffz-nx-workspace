c99bde57d51c42d606ffa8f02ffa562d9398a70c
Eric Büttner <eric.buettner@tuffz.com>
2023-11-05 17:39:45 +0100

    test(shared-ui-footer): add test for the backlink to tuffz.com

--- libs/shared/ui/footer/src/lib/shared-ui-footer.spec.tsx
+++ libs/shared/ui/footer/src/lib/shared-ui-footer.spec.tsx
@@ -3,8 +3,24 @@ import { render } from '@testing-library/react';
 import SharedUiFooter from './shared-ui-footer';
 
 describe('SharedUiFooter', () => {
-  it('should render successfully', () => {
+  it('should render correctly', () => {
     const { baseElement } = render(<SharedUiFooter />);
-    expect(baseElement).toBeTruthy();
+
+    // Check that the component renders without errors
+    expect(baseElement).toBeInTheDocument();
   });
+});
+
+describe('SharedUiFooter contains the tuffz.com link', () => {
+  const { getByText } = render(<SharedUiFooter />);
+
+  // Check that the "tuffz.com" link is present
+  const link = getByText('tuffz.com');
+  expect(link).toBeInTheDocument();
+
+  // Check that the link has the correct attributes
+  expect(link).toHaveAttribute('href', 'https://www.tuffz.com');
+  expect(link).toHaveAttribute('title', 'tuffz.com');
+  expect(link).toHaveAttribute('target', '_blank');
+  expect(link).toHaveAttribute('rel', 'noopener noreferrer');
 });
