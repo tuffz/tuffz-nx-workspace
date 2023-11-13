6895c6c3532a7e9089f1b2caeca9bb97e33299d1
Eric Büttner <eric.buettner@tuffz.com>
2023-11-13 23:57:06 +0100

    feat(shared-ui-social-media-icons)!: add new library
    BREAKING CHANGE: pump version to 1.0.0

---
+++ libs/shared/ui/social-media-icons/src/lib/ui-social-media-icons.tsx
@@ -0,0 +1,52 @@
+// Import SVG components for each platform
+import UiSocialMediaIconGitHub from './icons/github';
+
+export interface UiSocialMediaIconsProps {
+  url: string;
+}
+
+interface UiSocialMediaIconProps {}
+
+interface PlatformMapping {
+  component: (props: UiSocialMediaIconProps) => JSX.Element;
+  domain: string[];
+}
+
+const platformMappings: Record<string, PlatformMapping> = {
+  GitHub: {
+    component: UiSocialMediaIconGitHub,
+    domain: ['github.com', 'www.github.com'],
+  },
+  // Add more platforms as needed
+};
+
+const extractPlatformFromUrl = (url: string): string => {
+  const urlParts = url.split('/');
+  const domain = urlParts[2];
+
+  const platform = Object.entries(platformMappings).find(
+    ([key, { domain: keywords }]) =>
+      keywords.some((keyword) =>
+        new RegExp(`\\b${keyword}\\b`, 'i').test(domain),
+      ),
+  );
+
+  return platform ? platform[0] : 'unknown';
+};
+
+export function UiSocialMediaIcons(props: UiSocialMediaIconsProps) {
+  const platform = extractPlatformFromUrl(props.url);
+
+  if (platform in platformMappings) {
+    const { component: UiSocialMediaIcon } = platformMappings[
+      platform
+    ] as PlatformMapping;
+
+    return <UiSocialMediaIcon />;
+  } else {
+    // Handle case platform is not part of platformMapping
+    return null;
+  }
+}
+
+export default UiSocialMediaIcons;
