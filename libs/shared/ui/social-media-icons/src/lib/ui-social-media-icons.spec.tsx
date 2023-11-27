2173442e25fbdf20729cf646a19f6499462f9052
Eric Büttner <eric.buettner@tuffz.com>
2023-11-27 11:36:18 +0100

    feat(shared-ui-social-media-icons): add new library (#145)
    BREAKING CHANGE: pump version to 1.0.0

---
+++ libs/shared/ui/social-media-icons/src/lib/ui-social-media-icons.spec.tsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+
+import UiSocialMediaIcons from './ui-social-media-icons';
+
+describe('UiSocialMediaIcons', () => {
+  it('GitHub should render successfully', () => {
+    const { baseElement } = render(
+      <UiSocialMediaIcons url="http://github.com/tuffz" />,
+    );
+    expect(baseElement).toBeTruthy();
+  });
+});
