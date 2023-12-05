c0a18f641fd41b730bf4c407b4c03c0b820215e8
Eric Büttner <eric.buettner@tuffz.com>
2023-12-05 14:50:35 +0100

    feat(ericbuettner-com/career-timeline): introduce new library

---
+++ libs/ericbuettner-com/career-timeline/src/lib/ericbuettner-com/career-timeline.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface CareerTimelineProps {}
+
+export function CareerTimeline(props: CareerTimelineProps) {
+  return (
+    <div>
+      <h1>Welcome to CareerTimeline!</h1>
+    </div>
+  );
+}
+
+export default CareerTimeline;
