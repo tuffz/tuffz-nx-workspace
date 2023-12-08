f97b1d6629f01ef5d5a213a43022ac30753e09a2
Eric Büttner <eric.buettner@tuffz.com>
2023-12-09 00:14:22 +0100

    feat(utils-format-location)!: introduce `formatlocation` function
    BREAKING CHANGE: This function efficiently formats location objects, encompassing city, optional
    state, and country fields, rendering them into human-readable strings.

---
+++ libs/shared/utils/format-location/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'utils-format-location',
+  preset: '../../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../../coverage/libs/shared/utils/format-location',
+};
