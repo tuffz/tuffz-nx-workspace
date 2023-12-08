77c76ef321d24bcb6566f1c80f08690df50f290e
Eric Büttner <eric.buettner@tuffz.com>
2023-12-08 23:35:19 +0100

    refactor(ericbuettner-com/profile-snapshot): introduce interface and add logic to handle location

--- libs/ericbuettner-com/profile-snapshot/src/lib/ericbuettner-com/profile-snapshot.tsx
+++ libs/ericbuettner-com/profile-snapshot/src/lib/ericbuettner-com/profile-snapshot.tsx
@@ -2,33 +2,61 @@ import { UiImageEmbed } from '@tuffz/shared/ui/image-embed';
 import { UiSocialMediaIcons } from '@tuffz/shared/ui/social-media-icons';
 
 export interface ProfileSnapshotProps {
-  // firstname: string;
-  // surname: string;
-  // latest_job_position: string;
-  // latest_job_company_name: string;
-  // location_city: string;
-  // location_state?: string;
-  // location_country: string;
+  first_name: string;
+  last_name: string;
+  latest_job_position: string;
+  latest_job_company: string;
+  location_city: string;
+  location_state?: string;
+  location_country: string;
 }
 
 export function ProfileSnapshot(props: ProfileSnapshotProps) {
+  interface Location {
+    city: string;
+    state?: string;
+    country: string;
+  }
+
+  function formatLocation(location: Location): string {
+    const { city, state, country } = location;
+    let result = `${city},`;
+
+    if (state !== null && state !== undefined && state !== '') {
+      result += ` ${state},`;
+    }
+
+    result += ` ${country}`;
+
+    return result.trim();
+  }
+
+  // Retrieve city, state, and country from props or any other source
+  const location: Location = {
+    city: props.location_city,
+    state: props.location_state,
+    country: props.location_country,
+  };
+
+  const formattedLocation = formatLocation(location);
+
   return (
     <section className="flex flex-col sm:max-lg:flex-row md:justify-evenly items-center bg-white p-1 rounded-xl shadow-xl sm:m-4">
       <div className="shrink-0 mb-4 sm:mb-0 lg:m-4">
         <UiImageEmbed
           src="/assets/profile/eric-buettner.jpeg"
-          alt="Portrait of Eric Büttner"
+          alt={`Portrait of ${props.first_name} ${props.last_name}`}
           className="rounded-xl sm:rounded-xl sm:w-60 shadow-xl"
         />
       </div>
       <div className="text-center sm:grow lg:grow-0">
-        <h1 className="text-2xl py-2">Eric Büttner</h1>
+        <h1 className="text-2xl py-2">
+          {props.first_name} {props.last_name}
+        </h1>
         <p className="text-gray-700 py-2">
-          Head of Software Engineering at allmyhomes GmbH
+          {props.latest_job_position} at {props.latest_job_company}
         </p>
-        <p className="text-sm text-gray-500 py-2">
-          Eberswalde, Brandenburg, Germany
-        </p>
+        <p className="text-sm text-gray-500 py-2">{formattedLocation}</p>
         <div className="p-2">
           <p className="flex flex-wrap justify-center">
             <span className="px-1 mx-1 text-sm">
