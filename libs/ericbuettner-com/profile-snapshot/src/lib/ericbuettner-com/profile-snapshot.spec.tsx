7b7ec9e30e8ece856f9e9e1c845a4c02950bb3bb
Eric Büttner <eric.buettner@tuffz.com>
2023-12-08 23:11:18 +0100

    refactor(ericbuettner-com/profile-snapshot): update reference to profile picture (#223)
    Moved the profile image to a new directory and updated the reference in the component

--- libs/ericbuettner-com/profile-snapshot/src/lib/ericbuettner-com/profile-snapshot.spec.tsx
+++ libs/ericbuettner-com/profile-snapshot/src/lib/ericbuettner-com/profile-snapshot.spec.tsx
@@ -8,10 +8,7 @@ describe('ProfileSnapshot', () => {
     // Check that the image is rendered with the correct attributes
     const image = getByAltText('Portrait of Eric Büttner');
     expect(image).toBeInTheDocument();
-    expect(image).toHaveAttribute(
-      'src',
-      '/assets/personal-information/eric-buettner.jpeg',
-    );
+    expect(image).toHaveAttribute('src', '/assets/profile/eric-buettner.jpeg');
     expect(image).toHaveAttribute('alt', 'Portrait of Eric Büttner');
     expect(image).toHaveAttribute('title', 'Portrait of Eric Büttner');
   });
