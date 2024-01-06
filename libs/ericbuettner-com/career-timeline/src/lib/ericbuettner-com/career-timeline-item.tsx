8f43143100ace83d1acc11d074bf6712d3ab75c3
Eric Büttner <eric.buettner@tuffz.com>
2024-01-06 21:13:52 +0100

    refactor(career-timeline): update location handling by using "utils-location" library
    - Changed the 'location' property type in the CareerTimelineItemProps interface to 'keyof typeof
      CompanyLocation' for better type safety.
    - Updated the component logic to extract and format location information from the
      CompanyLocation object, resulting in more structured and readable code.

--- libs/ericbuettner-com/career-timeline/src/lib/ericbuettner-com/career-timeline-item.tsx
+++ libs/ericbuettner-com/career-timeline/src/lib/ericbuettner-com/career-timeline-item.tsx
@@ -1,12 +1,29 @@
+import {
+  Location,
+  formatLocationToString,
+  structureLocation,
+} from '@tuffz/utils-locations';
+
+import { CompanyLocation } from './enum/company-location';
+
 export interface CareerTimelineItemProps {
   position: string;
   company: string;
   date_start: string;
   date_end: string;
-  location: string;
+  location: keyof typeof CompanyLocation; // Use keyof to specify location as a valid key
 }
 
 export function CareerTimelineItem(props: CareerTimelineItemProps) {
+  const location: Location = {
+    city: CompanyLocation[props.location].city,
+    state: CompanyLocation[props.location].state,
+    country: CompanyLocation[props.location].country,
+  };
+
+  const structuredLocation = structureLocation(location);
+  const formattedLocation = formatLocationToString(structuredLocation);
+
   return (
     <div className="career-timeline-item my-3">
       <div className="career-company-position-item flex flex-col flex-grow">
@@ -19,7 +36,7 @@ export function CareerTimelineItem(props: CareerTimelineItemProps) {
           {props.date_start} - {props.date_end}
         </span>
         <span className="block text-sm text-gray-400 py-0.5">
-          {props.location}
+          {formattedLocation}
         </span>
       </div>
     </div>
