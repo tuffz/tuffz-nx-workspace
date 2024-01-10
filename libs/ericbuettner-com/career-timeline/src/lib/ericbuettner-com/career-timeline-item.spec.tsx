eeace3f9a6606ed3127607b9e803cc72eff0272a
Eric Büttner <eric.buettner@tuffz.com>
2024-01-11 00:49:19 +0100

    test(career-timeline-item): add test for CareerTimelineItem component
    refs #298

--- libs/ericbuettner-com/career-timeline/src/lib/ericbuettner-com/career-timeline-item.spec.tsx
+++ libs/ericbuettner-com/career-timeline/src/lib/ericbuettner-com/career-timeline-item.spec.tsx
@@ -1,10 +1,22 @@
 import { render } from '@testing-library/react';
 
-import { CareerTimeline } from './career-timeline';
+import { CareerTimelineItem } from './career-timeline-item';
 
-describe('CareerTimeline', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<CareerTimeline />);
-    expect(baseElement).toBeTruthy();
+const input = {
+  position: 'Software Engineer',
+  company: 'Example Inc',
+  date_start: new Date('May, 2021'),
+  date_end: new Date('Oct, 2023'),
+  location: 'BERLIN_GERMANY',
+};
+
+describe('CareerTimelineItem', () => {
+  it('renders correctly', () => {
+    const { getByText } = render(<CareerTimelineItem {...input} />);
+
+    expect(getByText(input.position)).toBeInTheDocument();
+    expect(getByText(input.company)).toBeInTheDocument();
+    expect(getByText('May 2021 - Oct 2023'));
+    expect(getByText('Berlin, Berlin, Germany')).toBeInTheDocument();
   });
 });
