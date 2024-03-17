b5ede3eae4ef736a84f872b8e09d584e3f2447dd
Eric Büttner <eric.buettner@tuffz.com>
2024-03-17 18:41:53 +0100

    fix(ericbuettner-com): change the output directory from `out` to `dist`

--- apps/ericbuettner-com/next.config.js
+++ apps/ericbuettner-com/next.config.js
@@ -7,12 +7,22 @@ const { composePlugins, withNx } = require('@nx/next');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  output: 'export',
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+
+  output: 'export',
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  distDir: '../../dist/apps/ericbuettner-com',
 };
 
 const plugins = [
