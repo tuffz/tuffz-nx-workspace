3af3c5adaf188d90b748e28d64767dbccee1a14c
Eric Büttner <eric.buettner@tuffz.com>
2024-04-01 19:58:08 +0200

    chore: update Jest preset configuration
    This commit updates the Jest preset configuration in jest.preset.js file.
    
    Changes include:
    - Enabling coverage collection with collectCoverage: true.
    - Specifying coverage reporters as ['lcov', 'clover', 'text', 'cobertura'].
    - Setting coverage thresholds for branches, functions, lines, and statements.
      - Branches: 80%
      - Functions: 80%
      - Lines: 80%
      - Statements: -10% (indicating no specific threshold for statements)
    
    These changes aim to improve code coverage reporting and ensure better test coverage for the
    project.

--- jest.preset.js
+++ jest.preset.js
@@ -3,5 +3,15 @@ const nxPreset = require('@nx/jest/preset').default;
 const path = require('path');
 module.exports = {
   ...nxPreset,
+  collectCoverage: true,
+  coverageReporters: ['lcov', 'clover', 'text', 'cobertura'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
   setupFilesAfterEnv: [path.resolve(__dirname, './setup-testing-library.js')],
 };
