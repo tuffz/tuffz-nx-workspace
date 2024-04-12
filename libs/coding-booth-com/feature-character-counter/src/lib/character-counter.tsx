4ed78d3ed69c7f7cb3f6c9b6b22c52bd3f3b5126
Eric Büttner <eric.buettner@tuffz.com>
2024-04-12 23:52:39 +0200

    feat(coding-booth-com-feature-character-counter): enhance UI for `CharacterCounter` component (WIP)
    This commit enhances the user interface (UI) for the `CharacterCounter` component in the
    `coding-booth-com-feature-character-counter` library.
    
    It introduces the following changes:
    - Added a new utility function `toKebabCase` to convert strings to kebab case.
    - Created a new internal component `CounterResult` to display the result of character counts.
    - Refactored the `CharacterCounter` component to utilize the newly added utility function and
    internal component for better code organization and readability.
    
    This commit is marked as Work In Progress (WIP) as it solely focuses on UI improvements and does not
    include functionality changes. The term "refactoring" might not fully capture the essence of the
    changes made, as it primarily enhances the UI without altering existing functionality.
    
    These changes aim to improve the user experience and maintainability of the `CharacterCounter`
    component.

--- libs/coding-booth-com/feature-character-counter/src/lib/character-counter.tsx
+++ libs/coding-booth-com/feature-character-counter/src/lib/character-counter.tsx
@@ -1,7 +1,70 @@
-export function CharacterCounter() {
+export function toKebabCase(str: string): string {
   return (
-    <div>
-      <h1>Welcome to CharacterCounter!</h1>
+    str
+      .match(/[A-Z]?[a-z]+|[0-9]+/g) // Split string into words or numbers
+      ?.map((word) => word.toLowerCase()) // Convert each word to lowercase
+      .join('-') ?? '' // Join words with hyphen
+  );
+}
+interface CounterResultProps {
+  length: number;
+  title: string;
+  index: number;
+}
+
+function CounterResult(props: CounterResultProps) {
+  let className =
+    'grow border bg-lime-50 border-lime-600 border-b-0 sm:border sm:border-t-0 p-3 m-2 mb-0 sm:mt-0';
+
+  if (props.index === 0) {
+    className += ' ml-0'; // Append class if first item
+  }
+  if (props.index === props.length - 1) {
+    className += ' mr-0'; // Append class if last item
+  }
+
+  return (
+    <div id={toKebabCase('result-' + props.title)} className={className}>
+      279 {props.title}
+    </div>
+  );
+}
+
+export function CharacterCounter() {
+  const counts = [
+    'characters',
+    // 'words',
+    // 'without whitespaces',
+    'paragraphs',
+  ];
+
+  return (
+    <div className="px-5">
+      <div className="pt-5">
+        <h1 className="text-4xl text-lime-700">Character counter</h1>
+        <p className="pt-3 text-base text-lime-600">
+          Characters, letters and word counter
+        </p>
+      </div>
+      <div className="flex flex-col-reverse pt-5 sm:flex-col">
+        <div id="textarea" className="bg-gray-100">
+          <textarea
+            id="message"
+            className="block h-[50vh] min-h-[25vh] w-full border border-lime-500 bg-lime-50 p-2.5 text-lime-700 outline-1 focus:outline-lime-500"
+            placeholder="Type or paste your content here and watch the character counter work its magic!"
+          ></textarea>
+        </div>
+        <div className="flex flex-row items-center justify-between">
+          {counts.map((title, index) => (
+            <CounterResult
+              key={index}
+              length={counts.length}
+              title={title}
+              index={index}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
