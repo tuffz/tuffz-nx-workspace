e62a09f482e1fa087306a0b477800768fa4c334c
Eric Büttner <eric.buettner@tuffz.com>
2024-04-21 00:43:09 +0200

    feat(character-counter): implement dynamic character counting functionality
    Enhance the CharacterCounterTextarea component to dynamically count the number of characters entered
    by the user. This improvement enables real-time updating of the character count as the user types or
    pastes content into the textarea.
    
    - Utilized React's useState hook to manage the character count state.
    - Implements a change event handler to update the character count whenever the content of the
      textarea changes.
    - Provides immediate feedback to users by displaying the current character count in the UI.

--- libs/coding-booth-com/feature-character-counter/src/lib/character-counter-textarea.tsx
+++ libs/coding-booth-com/feature-character-counter/src/lib/character-counter-textarea.tsx
@@ -1,15 +1,24 @@
 'use client';
 
+import { ChangeEvent, useState } from 'react';
+
 export function CharacterCounterTextarea() {
+  const [charCount, setCharCount] = useState(0);
+
+  const handleInputChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    const inputText = event.target.value;
+    setCharCount(inputText.length);
+  };
 
   return (
     <div className="w-full lg:flex lg:flex-col">
       <div className="rounded-t-md border border-gray-300 bg-gray-50 py-2 text-center text-sm text-gray-900 lg:text-base dark:border-gray-600  dark:bg-gray-700 dark:text-white">
-        <strong className="text-lime-500">0</strong> Characters
+        <strong className="text-lime-500">{charCount}</strong> Characters
       </div>
       <textarea
         className="block min-h-[50vh] w-full rounded-b-md border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-0 outline-lime-500 focus:border-lime-500 lg:grow lg:text-base dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400"
         placeholder="Type or paste your content here and watch the character counter work its magic!"
+        onChange={handleInputChange}
       ></textarea>
     </div>
   );
