71b19518ad2c23f9d5e88c3e2da1a09039de342f
Eric Büttner <eric.buettner@tuffz.com>
2024-04-21 00:31:55 +0200

    refactor(character-counter): add IntroSection component
    Add a new IntroSection component to the CharacterCounter feature. This component renders the
    introductory text for the character counter, providing information about its purpose and
    functionality.
    
    Included changes:
    - Introduced a new component to enhance the readability and maintainability of the
      CharacterCounter feature.
    - Improved code organization by separating the introductory section into its own component.

--- libs/coding-booth-com/feature-character-counter/src/lib/character-counter.tsx
+++ libs/coding-booth-com/feature-character-counter/src/lib/character-counter.tsx
@@ -1,36 +1,11 @@
 'use client';
 
+import { IntroSection } from './intro-section';
+
 export function CharacterCounter() {
   return (
     <section className="bg-base-100 mx-auto flex max-w-7xl flex-col items-center justify-center gap-16 p-8 lg:flex-row lg:items-stretch lg:gap-20  lg:py-20">
-      <div className="flex flex-col items-center justify-center gap-10 text-center lg:items-start lg:justify-normal lg:gap-14 lg:text-left">
-        <h1 className="text-4xl font-extrabold tracking-tight md:-mb-4 lg:text-6xl dark:text-white">
-          No more guesswork, count characters in a blink!
-        </h1>
-        <p className="text-lg leading-relaxed opacity-80 dark:text-white">
-          Introducing{' '}
-          <strong className="text-xl font-bold tracking-tight md:-mb-4 md:text-2xl lg:text-3xl">
-            CharacterCounter
-          </strong>{' '}
-          - your{' '}
-          <strong className="underline decoration-lime-500 decoration-4">
-            simple
-          </strong>{' '}
-          tool for counting characters. It's{' '}
-          <strong className="underline decoration-lime-500 decoration-4">
-            straightforward
-          </strong>
-          ,{' '}
-          <strong className="underline decoration-lime-500 decoration-4">
-            efficient
-          </strong>
-          , and gets the job done{' '}
-          <strong className="underline decoration-lime-500 decoration-4">
-            hassle-free
-          </strong>
-          !
-        </p>
-      </div>
+      <IntroSection />
 
       <div className="w-full lg:flex lg:flex-col">
         <div className="rounded-t-md border border-gray-300 bg-gray-50 py-2 text-center text-sm text-gray-900 dark:border-gray-600  dark:bg-gray-700 dark:text-white">
