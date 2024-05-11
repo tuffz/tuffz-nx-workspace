66395abc8328677d006a3a79f21eebfbdf31b42c
Eric Büttner <eric.buettner@tuffz.com>
2024-05-11 18:46:44 +0200

    refactor(coding-booth-com-feature-under-construction)!: move and rename library
    Changes included:
    - Moved files from "libs/coding-booth-com/under-construction/" to
      "libs/coding-booth-com/feature/under-construction"
    - Renamed library from "@tuffz/coding-booth/under-construction" to
      "@tuffz/coding-booth-com-feature-under-construction"
    
    BREAKING CHANGE: The import paths changed from "@tuffz/coding-booth/under-construction" to
    "@tuffz/coding-booth-com-feature-under-construction"

--- libs/coding-booth-com/feature/under-construction/jest.config.ts
+++ libs/coding-booth-com/feature/under-construction/jest.config.ts
@@ -1,12 +1,12 @@
 /* eslint-disable */
 export default {
   displayName: 'coding-booth-com-under-construction',
-  preset: '../../../jest.preset.js',
+  preset: '../../../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory:
-    '../../../coverage/libs/coding-booth-com/under-construction',
+    '../../../../coverage/libs/coding-booth-com/feature/under-construction',
 };
