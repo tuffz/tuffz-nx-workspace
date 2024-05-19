e1264ec242ef92dd10071992fd7d3535df8f91c7
Eric Büttner <eric.buettner@tuffz.com>
2024-05-19 15:42:03 +0200

    refactor(utm-parameter-url-builder): update placeholder handling and labels
    Changes included:
    - Modified `UtmParameterUrlBuilderInput` to append '*' to placeholders of required fields
    - Updated placeholders in `UtmParameterUrlBuilder` to no longer include '*' directly in the text
    
    This refactoring improves the clarity and consistency of required field indicators by handling
    the '*' programmatically.

--- libs/coding-booth-com/feature/utm-parameter-url-builder/src/lib/utm-parameter-url-builder-input.tsx
+++ libs/coding-booth-com/feature/utm-parameter-url-builder/src/lib/utm-parameter-url-builder-input.tsx
@@ -17,7 +17,7 @@ export function UtmParameterUrlBuilderInput({
     <div className="pb-4">
       <input
         className="block w-full rounded-md border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-0 outline-lime-500 focus:border-lime-500 lg:grow lg:text-base dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400"
-        placeholder={placeholder}
+        placeholder={required ? `${placeholder} *` : placeholder}
         type="text"
         id={id}
         required={required}
