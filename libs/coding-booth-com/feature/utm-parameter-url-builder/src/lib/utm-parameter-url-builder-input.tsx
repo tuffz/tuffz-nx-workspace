010a6ca0e517b87a1147d8b52cf470613b8def15
Eric Büttner <eric.buettner@tuffz.com>
2024-05-19 15:38:09 +0200

    refactor(utm-parameter-url-builder): add UTM Parameter URL Builder Input component
    Changes included:
    - Created `UtmParameterUrlBuilderInput` component
    - Added input field with customizable `id`, `label`, `placeholder`, and `required` properties
    - Included default styling for light and dark modes
    
    This refactoring introduces a new component to streamline the creation of input fields for UTM
    parameters in URLs.

---
+++ libs/coding-booth-com/feature/utm-parameter-url-builder/src/lib/utm-parameter-url-builder-input.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+interface UtmParameterUrlBuilderInputProps {
+  id: string;
+  label: string;
+  placeholder: string;
+  required: boolean;
+}
+
+export function UtmParameterUrlBuilderInput({
+  id,
+  label,
+  placeholder,
+  required,
+}: UtmParameterUrlBuilderInputProps) {
+  return (
+    <div className="pb-4">
+      <input
+        className="block w-full rounded-md border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-0 outline-lime-500 focus:border-lime-500 lg:grow lg:text-base dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400"
+        placeholder={placeholder}
+        type="text"
+        id={id}
+        required={required}
+      />
+      <div className="px-2.5">
+        <label
+          htmlFor={id}
+          className="pt-2 text-sm leading-relaxed opacity-80 dark:text-white"
+        >
+          {label}
+        </label>
+      </div>
+    </div>
+  );
+}
+
+export default UtmParameterUrlBuilderInput;
--- libs/coding-booth-com/feature/utm-parameter-url-builder/src/lib/utm-parameter-url-builder.tsx
+++ libs/coding-booth-com/feature/utm-parameter-url-builder/src/lib/utm-parameter-url-builder.tsx
@@ -1,6 +1,53 @@
 'use client';
 
+import { UtmParameterUrlBuilderInput as InputField } from './utm-parameter-url-builder-input';
+
 export function UtmParameterUrlBuilder() {
+  const inputFields = [
+    {
+      id: 'website-url',
+      label: 'The full website URL (e.g. https://www.tuffz.com)',
+      placeholder: 'website URL *',
+      required: true,
+    },
+    {
+      id: 'campaign-id',
+      label: 'The ads campaign id',
+      placeholder: 'campaign ID',
+      required: false,
+    },
+    {
+      id: 'campaign-source',
+      label: 'The referrer (e.g. google, newsletter)',
+      placeholder: 'campaign source *',
+      required: true,
+    },
+    {
+      id: 'campaign-medium',
+      label: 'The marketing medium (e.g. cpc, banner, email)',
+      placeholder: 'campaign medium *',
+      required: true,
+    },
+    {
+      id: 'campaign-name',
+      label: 'Product, promo code, or slogan (e.g. summer_sale)',
+      placeholder: 'campaign name *',
+      required: true,
+    },
+    {
+      id: 'campaign-term',
+      label: 'Identify the paid keywords',
+      placeholder: 'campaign term',
+      required: false,
+    },
+    {
+      id: 'campaign-content',
+      label: 'Use to differentiate ads',
+      placeholder: 'campaign content',
+      required: false,
+    },
+  ];
+
   return (
     <>
       <h2 className="pb-4 text-2xl font-extrabold tracking-tight lg:text-2xl dark:text-white">
@@ -11,129 +58,15 @@ export function UtmParameterUrlBuilder() {
         will be generated for you.
       </p>
 
-      <div className="pb-4">
-        <input
-          className="block w-full rounded-md border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-0 outline-lime-500 focus:border-lime-500 lg:grow lg:text-base dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400"
-          placeholder="website URL *"
-          type="text"
-          id="website-url"
-          required
+      {inputFields.map((field) => (
+        <InputField
+          key={field.id}
+          id={field.id}
+          label={field.label}
+          placeholder={field.placeholder}
+          required={field.required}
         />
-        <div className="px-2.5">
-          <label
-            htmlFor="website-url"
-            className="pt-2 text-sm leading-relaxed opacity-80 dark:text-white"
-          >
-            The full website URL (e.g. <strong>https://www.tuffz.com</strong>)
-          </label>
-        </div>
-      </div>
-      <div className="pb-4">
-        <input
-          className="block w-full rounded-md border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-0 outline-lime-500 focus:border-lime-500 lg:grow lg:text-base dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400"
-          placeholder="campaign ID"
-          type="text"
-          id="campaign-id"
-          // required
-        />
-        <div className="px-2.5">
-          <label
-            htmlFor="campaign-id"
-            className="pt-2 text-sm leading-relaxed opacity-80 dark:text-white"
-          >
-            The ads campaign id
-          </label>
-        </div>
-      </div>
-      <div className="pb-4">
-        <input
-          className="block w-full rounded-md border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-0 outline-lime-500 focus:border-lime-500 lg:grow lg:text-base dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400"
-          placeholder="campaign source *"
-          type="text"
-          id="campaign-source"
-          required
-        />
-        <div className="px-2.5">
-          <label
-            htmlFor="campaign-source"
-            className="pt-2 text-sm leading-relaxed opacity-80 dark:text-white"
-          >
-            The referrer (e.g. <strong>google</strong>,{' '}
-            <strong>newsletter</strong>)
-          </label>
-        </div>
-      </div>
-      <div className="pb-4">
-        <input
-          className="block w-full rounded-md border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-0 outline-lime-500 focus:border-lime-500 lg:grow lg:text-base dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400"
-          placeholder="campaign medium *"
-          type="text"
-          id="campaign-medium"
-          required
-        />
-        <div className="px-2.5">
-          <label
-            htmlFor="campaign-medium"
-            className="pt-2 text-sm leading-relaxed opacity-80 dark:text-white"
-          >
-            The marketing medium (e.g. <strong>cpc</strong>,{' '}
-            <strong>banner</strong>, <strong>email</strong>)
-          </label>
-        </div>
-      </div>
-      <div className="pb-4">
-        <input
-          className="block w-full rounded-md border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-0 outline-lime-500 focus:border-lime-500 lg:grow lg:text-base dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400"
-          placeholder="campaign name *"
-          type="text"
-          id="campaign-name"
-          required
-        />
-        <div className="px-2.5">
-          <label
-            htmlFor="campaign-name"
-            className="pt-2 text-sm leading-relaxed opacity-80 dark:text-white"
-          >
-            Product, promo code, or slogan (e.g. <strong>summer_sale</strong>)
-          </label>
-        </div>
-      </div>
-
-      <div className="pb-4">
-        <input
-          className="block w-full rounded-md border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-0 outline-lime-500 focus:border-lime-500 lg:grow lg:text-base dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400"
-          placeholder="campaign term"
-          type="text"
-          id="campaign-term"
-          // required
-        />
-        <div className="px-2.5">
-          <label
-            htmlFor="campaign-term"
-            className="pt-2 text-sm leading-relaxed opacity-80 dark:text-white"
-          >
-            Identify the paid keywords
-          </label>
-        </div>
-      </div>
-
-      <div className="pb-4">
-        <input
-          className="block w-full rounded-md border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-0 outline-lime-500 focus:border-lime-500 lg:grow lg:text-base dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400"
-          placeholder="campaign content"
-          type="text"
-          id="campaign-content"
-          // required
-        />
-        <div className="px-2.5">
-          <label
-            htmlFor="campaign-content"
-            className="pt-2 text-sm leading-relaxed opacity-80 dark:text-white"
-          >
-            Use to differentiate ads
-          </label>
-        </div>
-      </div>
+      ))}
     </>
   );
 }
