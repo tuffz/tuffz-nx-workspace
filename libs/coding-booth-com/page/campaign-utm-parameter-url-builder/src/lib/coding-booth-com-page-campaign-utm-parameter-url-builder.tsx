784b0b1fecba4fa995fec931c2ba6a9223bdd232
Eric Büttner <eric.buettner@tuffz.com>
2024-05-20 16:09:39 +0200

    refactor(page-campaign-utm-parameter-url-builder): extract introduction to separate component
    Included changes:
    - Created `Introduction` component for the introduction section
    - Updated the campaign UTM parameter URL builder page to use the new `Introduction` component
    
    This refactoring improves code organization and readability by extracting the introduction text into
    a separate component.

--- libs/coding-booth-com/page/campaign-utm-parameter-url-builder/src/lib/coding-booth-com-page-campaign-utm-parameter-url-builder.tsx
+++ libs/coding-booth-com/page/campaign-utm-parameter-url-builder/src/lib/coding-booth-com-page-campaign-utm-parameter-url-builder.tsx
@@ -3,48 +3,14 @@ import {
   UtmParameterUrlBuilderResult,
 } from '@tuffz/coding-booth-com-feature-utm-parameter-url-builder';
 
+import { Introduction } from './introduction';
+
 export function CodingBoothComPageCampaignUtmParameterUrlBuilder() {
   return (
     <section className="lg:py-20">
       <div className="container mx-auto">
         <div className="flex flex-col items-center justify-center gap-16 p-8 lg:flex-row lg:items-start lg:gap-20">
-          <div className="flex flex-col items-center justify-center gap-10 text-center lg:items-start lg:gap-14 lg:text-left">
-            <h1 className="text-4xl font-extrabold tracking-tight md:-mb-4 lg:text-6xl dark:text-white">
-              Guaranteed{' '}
-              <strong className="underline decoration-lime-500 decoration-8">
-                error-free
-              </strong>{' '}
-              UTM tracking for precise insights with ease!
-            </h1>
-            <p className="text-left text-lg leading-relaxed opacity-80 md:text-center lg:text-left dark:text-white">
-              Say goodbye to tracking errors and hello to{' '}
-              <strong className="underline decoration-lime-500 decoration-4">
-                precise insights
-              </strong>{' '}
-              effortlessly! <br className="hidden sm:block lg:hidden" />{' '}
-              <br className="hidden xl:block" />
-              Our{' '}
-              <strong className="text-xl font-bold tracking-tight md:-mb-4">
-                Campaign UTM Parameter URL Builder
-              </strong>
-              ,{' '}
-              <strong className="underline decoration-lime-500 decoration-4">
-                simplifies
-              </strong>{' '}
-              your URL creation process. No more worrying about typos or missing
-              parameters. <br className="hidden sm:block lg:hidden" />{' '}
-              <br className="hidden xl:block" />
-              Easily generate{' '}
-              <strong className="underline decoration-lime-500 decoration-4">
-                all necessary and optional
-              </strong>{' '}
-              parameters{' '}
-              <strong className="underline decoration-lime-500 decoration-4">
-                with confidence
-              </strong>
-              .
-            </p>
-          </div>
+          <Introduction />
 
           <div className="lg:w-full lg:min-w-96">
             <UtmParameterUrlBuilder />
