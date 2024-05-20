3760ae9efe369f212059bd69821f656850aed26f
Eric Büttner <eric.buettner@tuffz.com>
2024-05-20 15:22:59 +0200

    refactor(utm-parameter-url-builder): update input field hover background color
    Changes included:
    - Added `hover:bg-gray-50` to input field for light mode
    - Added `dark:hover:bg-neutral-800` to input field for dark mode
    
    This change enhances the user interface by providing visual feedback on hover.

--- libs/coding-booth-com/feature/utm-parameter-url-builder/src/lib/utm-parameter-url-builder-input.tsx
+++ libs/coding-booth-com/feature/utm-parameter-url-builder/src/lib/utm-parameter-url-builder-input.tsx
@@ -11,7 +11,7 @@ export function UtmParameterUrlBuilderInput({
   return (
     <div className="pb-4">
       <input
-        className="block w-full rounded-md border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-0 outline-lime-500 focus:border-lime-500 lg:grow lg:text-base dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400"
+        className="block w-full rounded-md border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-0 outline-lime-500 hover:bg-gray-50 focus:border-lime-500 lg:grow lg:text-base dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:hover:bg-neutral-800"
         placeholder={required ? `${placeholder} *` : placeholder}
         type="text"
         id={id}
