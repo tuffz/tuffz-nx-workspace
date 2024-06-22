5640eed97fc76f3c44d732aa44a4642cd318f776
Eric Büttner <eric.buettner@tuffz.com>
2024-06-22 11:56:45 +0200

    feat(coding-booth-com-page-not-found)!: introduce Page 404 Not Found library
    Included changes:
    
    - Created new ESLint configuration for the library
    - Added `CHANGELOG.md` and `README.md` files
    - Configured Jest with a custom `jest.config.ts` file
    - Defined project settings in `project.json`
    - Added main `index.ts` export file
    - Implemented "PageNotFound" component with styles and messages
    - Created snapshot test for "PageNotFound" component
    - Configured TypeScript with `tsconfig.json`, `tsconfig.lib.json`, and `tsconfig.spec.json`
    - Updated `tsconfig.base.json` with new library path

---
+++ libs/coding-booth-com/page/not-found/README.md
@@ -0,0 +1,7 @@
+# Coding-Booth.com Page: Not Found - Error 404
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test coding-booth-com-page-not-found` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/coding-booth-com/page/not-found/src/lib/page-not-found.spec.tsx
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+
+import { PageNotFound } from './page-not-found';
+
+describe('PageNotFound', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<PageNotFound />);
+    expect(baseElement).toMatchSnapshot();
+  });
+});
