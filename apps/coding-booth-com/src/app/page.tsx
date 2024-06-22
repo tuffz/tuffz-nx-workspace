054ab02685e5657bd6db244611d4f2ea5de35b58
Eric Büttner <eric.buettner@tuffz.com>
2024-06-22 12:11:52 +0200

    feat(coding-booth-com): introduce canonical URL metadata to HomePage
    Introduced canonical UTL metadata in `page.tsx` to specify the primary URL for search engines

--- apps/coding-booth-com/src/app/page.tsx
+++ apps/coding-booth-com/src/app/page.tsx
@@ -1,5 +1,11 @@
 import { CodingBoothComPageHome } from '@tuffz/coding-booth-com-page-home';
 
+export const metadata = {
+  alternates: {
+    canonical: '/',
+  },
+};
+
 export default function HomePage() {
   return <CodingBoothComPageHome />;
 }
