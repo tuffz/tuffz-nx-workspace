75c5fb64438b36c3cbdcc0823524d50f16e422eb
Eric Büttner <eric.buettner@tuffz.com>
2024-07-21 17:43:25 +0200

    docs(core): clarify comments on import statement usage for "testing-library/jest-dom"
    Improved the documentation comments in `testing-library-jest-dom.js` to clarify the rationale for
    using require statements and the potential issues with import statements in certain project
    configurations.
    
    - Improved the explanation for using require to prevent issues with some project' default settings
    - Clearly separated the SyntaxError example from the explanatory text
    - Enhanced the readability of the instructions on using import statements with appropiate
      configuration adjustments

--- scripts/testing-library-jest-dom.js
+++ scripts/testing-library-jest-dom.js
@@ -1,6 +1,9 @@
-// use require to prevent issues with some project types default settings
-// otherwise will throw error:
-//  SyntaxError: Cannot use import statement outside a module
-// you can use import just will need to adjust the configs to make sure they are transpiles
-// issues with js nest node workspace, seems fine for angular and react.
+// Use require to prevent issues with some project types' default settings.
+// Otherwise, the following error will occur:
+//
+// SyntaxError: Cannot use import statement outside a module
+//
+// You can use import statements, but you'll need to adjust the configurations
+// to ensure they are transpiled. This issue is specific to some JS Nest Node
+// workspaces, while it seems fine for Angular and React projects.
 require('@testing-library/jest-dom');
